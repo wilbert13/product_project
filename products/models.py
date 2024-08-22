85f19b6abbb428ce8babc9049c7d4bc2a30f1e82
wilbert13 <wilberthvers05@gmail.com>
2024-08-21 18:55:16 -0600

    Add files via upload

---
+++ products/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Product(models.Model):
+    LOCAL = 'local'
+    INTERNATIONAL = 'international'
+    PRODUCT_TYPE_CHOICES = [
+        (LOCAL, 'Local'),
+        (INTERNATIONAL, 'International'),
+    ]
+
+    code = models.CharField(max_length=20, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_type = models.CharField(max_length=20, choices=PRODUCT_TYPE_CHOICES)
+
+    def __str__(self):
+        return self.code
---
+++ products/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductCreateView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
